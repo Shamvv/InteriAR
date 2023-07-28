660b801f53c4480d13e13a8c1e925ff3f564b67f
shamvv <taichiikou@gmail.com>
2023-07-28 20:30:41 +0530

    Added UnityEngine.EventSystems to InputManager.cs

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
 
 public class InputManager : MonoBehaviour
