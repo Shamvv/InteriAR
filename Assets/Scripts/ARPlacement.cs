dc470348d7bf63472c4107867ec37ff71a070b4c
shamvv <taichiikou@gmail.com>
2023-07-29 18:51:03 +0530

    Updated spawning feature

--- Assets/Scripts/ARPlacement.cs
+++ Assets/Scripts/ARPlacement.cs
@@ -7,24 +7,32 @@ using UnityEngine.XR.ARSubsystems;
 public class ARPlacement : MonoBehaviour
 {
 
-    public GameObject arObjectToSpawn;
+    public GameObject UIArrows;
+
+   // public GameObject arObjectToSpawn;
     public GameObject placementIndicator;
     private GameObject spawnedObject;
     private Pose PlacementPose;
     private ARRaycastManager aRRaycastManager;
     private bool placementPoseIsValid = false;
 
+    public GameObject[] arModels;
+    int modelIndex = 0;
+
     void Start()
     {
         aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+        UIArrows.SetActive(false);
+
     }
 
     // need to update placement indicator, placement pose and spawn 
     void Update()
     {
-        if(spawnedObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        if (spawnedObject == null && placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            ARPlaceObject();
+            ARPlaceObject(modelIndex);
+            UIArrows.SetActive(true);
         }
 
 
@@ -35,7 +43,7 @@ public class ARPlacement : MonoBehaviour
     }
     void UpdatePlacementIndicator()
     {
-        if(spawnedObject == null && placementPoseIsValid)
+        if (spawnedObject == null && placementPoseIsValid)
         {
             placementIndicator.SetActive(true);
             placementIndicator.transform.SetPositionAndRotation(PlacementPose.position, PlacementPose.rotation);
@@ -53,17 +61,45 @@ public class ARPlacement : MonoBehaviour
         aRRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
 
         placementPoseIsValid = hits.Count > 0;
-        if(placementPoseIsValid)
+        if (placementPoseIsValid && spawnedObject == null)
         {
             PlacementPose = hits[0].pose;
         }
     }
 
-    void ARPlaceObject()
+    void ARPlaceObject(int id)
     {
-        spawnedObject = Instantiate(arObjectToSpawn, PlacementPose.position, PlacementPose.rotation);
+        for(int i = 0; i < arModels.Length; i++)
+        {
+            if(i == id)
+            {
+                GameObject clearUp = GameObject.FindGameObjectWithTag("ARMultiModel");
+                Destroy(clearUp);
+                spawnedObject = Instantiate(arModels[i], PlacementPose.position, PlacementPose.rotation); 
+            }
+        }
+
+       
     }
 
+    public void ModelChangeRight()
+    {
+        if (modelIndex < arModels.Length - 1)
+            modelIndex++;
+        else
+            modelIndex = 0;
+
+        ARPlaceObject(modelIndex);
+    }
+    public void ModelChangeLeft()
+    {
+        if (modelIndex > 0)
+            modelIndex--;
+        else
+            modelIndex = arModels.Length - 1;
+
+        ARPlaceObject(modelIndex);
+    }
 
 }
 
